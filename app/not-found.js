6a6dec77d6fc7fd51b8d7e072d08d86c0e802421
nikitha <nikithametta0904@gmail.com>
2024-05-15 18:48:16 +0530

    modified

--- app/not-found.js
+++ app/not-found.js
@@ -1,4 +1,3 @@
-// pages/_404.js
 
 import React from "react";
 import Link from "next/link";
@@ -9,7 +8,7 @@ const NotFoundPage = () => {
       <div className="container mx-auto text-center"> 
         <h1 className="text-3xl font-bold mb-4">Page Not Found</h1>
         <p className="text-lg text-gray-700">
-          Sorry, the page you"re looking for does not exist. Please check the URL or use the button below to return to the main page.
+          Sorry, the page you&apos;re looking for does not exist. Please check the URL or use the button below to return to the main page.
         </p>
         
         <Link href="/">
